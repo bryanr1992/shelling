63793ed2ca082f1b345cd75a55b745df8dfd2e60
Never Tech <bryan.rivas05@gmail.com>
2026-08-06 18:38:23 -0400

    initial implementation of exit

--- app/main.py
+++ app/main.py
@@ -5,7 +5,14 @@ def main():
     #RPL loop
     while True:
         sys.stdout.write("$ ")
+
         command = input()
+        
+        #if command == exit stop the shell. temp solution using lower case for now until
+        #I learn more about shell behavior
+        if command.lower() == "exit":
+            break
+        
         print(f"{command}: command not found")
         pass
 
