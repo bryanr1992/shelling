f7a9a4f67da3eee94b6941366de36e7207adcea2
Never Tech <bryan.rivas05@gmail.com>
2026-08-11 14:02:34 -0400

    updated code to use pathlib instead of just OS functions

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import sys
 import os
+from pathlib import Path
 
-path = os.environ.get('PATH','')
-dirs = path.split(os.pathsep)
+path_var = os.environ.get('PATH','')
+dirs = [Path(p) for p in path_var.split(os.pathsep)]
 
 BUILT_INS = {"exit": True,
              "echo": True,
@@ -20,10 +21,11 @@ def handle_command(command):
                     #check for execute permision
                     #print <command> full path if execute pernussion
                     #return
-                file = os.path.join(dir, command[1])
-                if os.path.exists(file):
-                    if os.access(file, os.X_OK):
-                        print(f"{command[1]} is {file}")
+                dir = dir / command[1]
+                print(dir)
+                if dir.is_file():
+                    if os.access(dir, os.X_OK):
+                        print(f"{command[1]} is {dir}")
                         return
                         
             print(f"{command[1]}: not found")
