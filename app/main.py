2926010081b580fa5bb15ff35e65f810262983ac
Never Tech <bryan.rivas05@gmail.com>
2026-08-13 10:54:49 -0400

    Refactoring

--- app/main.py
+++ app/main.py
@@ -11,62 +11,69 @@ BUILT_INS = {"exit": True,
               "type": True }
 
 def is_executable(path):
-
+    """Determines if we have a file instead of a dir
+       and if the file has execute permision
+    """
     if path.is_file():
         if os.access(path, os.X_OK):
             return True
     return False
 
 
-def handle_command(command):
-    if command[0] == "echo":
-        print(" ".join(command[1:]))
-    elif command[0] == "type":
-        if command[1] in BUILT_INS:
-            print(f"{command[1]} is a shell builtin")
-        else:
+def handle_command(cmd, args):
+    if cmd == "echo":
+        print(" ".join(args))
+    elif cmd == "type":
+        if args and args[0] in BUILT_INS:
+            print(f"{args[0]} is a shell builtin")
+        elif args:
             #Check if a file with the command name exist,
                 #check for execute permision
                     #print <command> full path if execute pernussion
                     #return
             for dir in dirs:
-                dir = dir / command[1]
+                dir = dir / args[0]
                 if is_executable(dir):
-                    print(f"{command[1]} is {dir}")
+                    print(f"{args[0]} is {dir}")
                     return
+            print(f"{args[0]}: not found") #prints when args is not empty and the built in command does not exist
                         
-            print(f"{command[1]}: not found")
+        else:
+            print(f"{args}: not found")#prints when args is empty
     else:
         for dir in dirs:
-            dir = dir / command[0]
+            dir = dir / cmd
             if is_executable(dir):
                 try:
-                    subprocess.run(command)
+                    subprocess.run([cmd])
+                    return
                 except subprocess.CalledProcessError as e:
                     print(f"Command failed with exit code {e.returncode}")
                     print(f"Error message: {e.stderr}")
-        print(f"{command[0]}: command not found")#tmp command not found
+        print(f"{cmd}: command not found")#tmp command not found
 
 def main():
     #REPL loop
     while True:
         sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-        command = input()
-        
-        #if command == exit stop the shell. temp solution using lower case for now until
-        #I learn more about shell behavior
+        command = input().strip()
 
+        if not command:
+            continue
         #naive parsing (not sure, but probably)
 
         command = command.split(" ")
 
-        if command[0] == "exit":
+        cmd = command[0]
+
+        args = command[1:] if len(command) > 1 else ""
+
+        if cmd == "exit":
             break
-        elif command[0] == "":
-            continue
         else:
-            handle_command(command)
+            handle_command(cmd,args)
         pass
 
 
